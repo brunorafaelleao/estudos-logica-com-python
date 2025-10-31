2c362cb438eeaff972dc2795014404fa20ab4413
Bruno Leão <132297721+brunorafaelleao@users.noreply.github.com>
2025-10-31 16:48:25 -0400

    edição do código para criar variavel e lista

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,15 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+print("Trabalhando com variáveis python")
+variavel = input("Digite um texto para armazenar na variável: ")
+print(f"A variável criada foi uma string de valor: {variavel}")
+print("Agora vamos criar uma lista para armazenar várias strings.")
+lista = []
+while True:
+    lista_item = input("Digite uma string para adicionar à lista (ou 'sair' para finalizar): ")
+    if lista_item.lower() == 'sair':
+        break
+    lista.append(lista_item)
+
+print("A lista criada contém as seguintes strings:")
+for item in lista:
+    print(f"- {item}")
